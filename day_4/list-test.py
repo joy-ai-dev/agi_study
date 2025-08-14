5b71b4cf7ff1d1448275b08e962337a3ca890ca5
Joy <joyroshan@gmail.com>
2025-08-14 11:43:50 +0530

    Committing day 4

---
+++ day_4/list-test.py
@@ -0,0 +1,12 @@
+mylist = ["apple", "banana", "cherry"]
+
+for fruits in mylist:
+    print (fruits)
+
+print(len(mylist))
+print(type(mylist))
+print(mylist[1])
+newlist = mylist[0:2]
+newlist[1] = "orange"
+print(mylist)
+print(newlist)
